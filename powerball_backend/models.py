9ab64ca15d7a28c3b631f9f884b9ff068bec77b4
Julio Cesar <julio.cesar@2xt.com.br>
2015-12-15 23:37:06 -0200

    adding the model. initially the same structure than ana

--- powerball_backend/models.py
+++ powerball_backend/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from datetime import date
+from jsonfield import JSONField
 
-# Create your models here.
+
+class Prize(models.Model):
+    draw_date = models.DateField(default=date.today)
+    code = models.CharField(max_length=100)
+
+
+class Ticket(models.Model):
+    prize = models.ForeignKey(Prize, related_name="tickets")
+    numbers = JSONField()
+    winning = models.NullBooleanField()
+    code = models.CharField(max_length=100)
+
+    def drawed(self):
+        return self.winning is not None
