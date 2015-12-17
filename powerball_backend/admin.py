51895dca08e73625fa76788ae4a13f48f62f4281
Julio Cesar <julio.cesar@2xt.com.br>
2015-12-17 10:41:15 -0200

    creating support for django admin

---
+++ powerball_backend/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from powerball_backend.models import Ticket, Prize
+
+admin.site.register(Ticket)
+admin.site.register(Prize)
--- powerball_backend/models.py
+++ powerball_backend/models.py
@@ -7,6 +7,9 @@ class Prize(models.Model):
     draw_date = models.DateField(default=date.today)
     code = models.CharField(max_length=100)
 
+    def __str__(self):
+        return "#{} Draw Date {}".format(self.pk, self.draw_date.strftime('%Y-%m-%d'))
+
 
 class Ticket(models.Model):
     prize = models.ForeignKey(Prize, related_name="tickets")
@@ -16,3 +19,6 @@ class Ticket(models.Model):
 
     def drawed(self):
         return self.winning is not None
+
+    def __str__(self):
+        return "Ticket {} - Prize {}".format(str(self.numbers), str(self.prize))
