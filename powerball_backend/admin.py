ffd32dd356caabf0e77f6327cfd1e2abe1f6580f
Julio Cesar <julio.cesar@2xt.com.br>
2015-12-17 11:19:55 -0200

    facilitating viewing of tickets from prizes on admin

--- powerball_backend/admin.py
+++ powerball_backend/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from powerball_backend.models import Ticket, Prize
 
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+
+
+class PrizeAdmin(admin.ModelAdmin):
+    inlines = [
+        TicketInline,
+    ]
+
 admin.site.register(Ticket)
-admin.site.register(Prize)
+admin.site.register(Prize, PrizeAdmin)
